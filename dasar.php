c638a72470ccacd2c0b13a3464421ce12ffa4728
Dwigitaanggraeni <110141438+Dwigitaanggraeni@users.noreply.github.com>
2022-09-09 19:24:44 +0700

    Add files via upload

---
+++ dasar.php
@@ -0,0 +1,23 @@
+<?php
+    //Variable
+    // $nama; Contoh Salah (Karena diawali angka)
+    // $nama;  Contoh Salah (Ada Simbol Ditengah)
+    // nama; contoh salah (tidak ada simbol $)
+    $nama_panjang = "dwii woi";
+    $usia = 17;
+    echo "<br><br>";
+
+    echo $nama_panjang . $usia;
+    // Tipe data (Integer, Float, String)
+
+
+
+
+
+    // (Boolean, Array)
+
+
+
+
+
+    // Array String
